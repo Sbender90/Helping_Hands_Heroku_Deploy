3d0af912d44a84462631bf5a0cd99069cd0c06c7
Steven Bender <sbender1990@gmail.com>
2018-05-17 17:03:24 -0500

    heroku test

--- app/config/connection.js
+++ app/config/connection.js
@@ -5,10 +5,10 @@ if (process.env.JAWSDB_URL){
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: "wm63be5w8m7gs25a.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
-        user: "	y6lh7drq900uvlm0",
-        password: "wnctnnrxcld1sllx",
-        database: "ovvj04fmoxlymsxf",
+        host: "wm63be5w8m7gs25a.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        user: "	wulhbntwif2nwt0j",
+        password: "gd8tso2xu104y3b0",
+        database: "uinavw5a5l9jludb",
     });
 };
 
