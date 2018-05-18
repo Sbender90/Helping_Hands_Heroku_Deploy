2ef25a736e1f7c67d10ec848ebf477e8b84f5017
Steven Bender <sbender1990@gmail.com>
2018-05-18 17:48:42 -0500

    buttons still not functional

--- app/routes/volunteer-apiroutes.js
+++ app/routes/volunteer-apiroutes.js
@@ -61,124 +61,26 @@ module.exports = function(app) {
     });
   });
 
+  app.post("/api/going", function(req, res) {
+    console.log("I'm going: ", req.body);
+    Volunteer_Events.create({
+      vol_id: req.body.dbCurrentUser
+    });
+  });
 
-//   app.get("/api/posts/:user", function (req, res) {
+app.get("/api/:user"), function(req, res) {
+  console.log("User Id", req.id);
+  db.Users.findOne({
+      where: {
+        email: req.body.email,
+        hh_role: req.body.hh_role
+    }
+  }).then(function(dbCurrentUser) {
+    res.json(dbCurrentUser);
+    console.log("Current User: ", dbCurrentUser);
 
-//     // console.log("vol_events get: ", res);
-//   // console.log(res)
-
-//   db.Volunteer_Events.findAll({
-//     where: {
-//       vol_id: req.params.user_id
-//     }
-//   }).then(function(dbevents){
-//     console.log(dbevents)
-//     res.json(dbevents);
-//   })
-// });
-  
-   
-  //tutor function
-//   app.get("/api/posts/:user", function (req, res) {
-
-//     // console.log("vol_events get: ", res);
-//   // console.log(res)
-//   var query={vol_id: 2};
-//   db.Volunteer_Events.findAll({
-//     where: {
-//       id: req.params.vol_id
-//     },
-//     include:[{model:db.Events}]
-//   }).then(function(dbevents){
-//     console.log(dbevents)
-//     res.json(dbevents);
-//   })
-// });
-
-
-
-
-    //     where: {
-    //         vol_id: 2 //(req.params.user, I assume)
-    //     }
-    // }).then(function (req,res) {
-    //     console.log("post res:"+ res);
-    //     // db.Events.findAll({
-    //     //     where: {
-    //     //         [Op.or]: [{ vol_id }, { event_id }] //or some manipulation of it
-    //     //     }
-    //     // }).then(function (dbEvents) {
-    //     //     console.log(dbEvents);
-    //     //     res.json(dbEvents);
-    //     // });
-  //  //});
-
-
-
-  // app.get("/api/posts/:user", function(req, res) {
-  //   // console.log("vol_events get: ", res);
-  //   db.Volunteer_Events.findAll ({
-  //     where: {
-  //       vol_id: 2
-  //     },
-  //     include: [db.Events]
-  //   }).then(function(dbVolunteer_Events){
-  //     res.json(dbVolunteer_Events);
-  //     // console.log("got the Volunteer_Events: ", dbVolunteer_Events);
-  //   });
-  // });
-
-  // app.delete("/api/posts/:user/:id", function(req, res) {
-  //   db.Volunteer_Events.destroy({
-  //     where: {
-  //       id: req.params.vol_id
-  //     }
-  //   }).then(function(dbPost) {
-  //     res.json(dbPost);
-  //   });
-  // });
-
-
-  // app.get("/api/posts/:user", function(req, res) {
-  //   // console.log("vol_events get: ", res);
-  //   db.Events.findAll ({
-      
-  //     include: [db.Volunteer_Events]
-  //   }).then(function(dbVolunteer_Events){
-  //     res.json(dbVolunteer_Events);
-  //     // console.log("got the Volunteer_Events: ", dbVolunteer_Events);
-  //   });
-  // });
-  // app.get("/api/posts/:user", function(req, res) {
-  //   // not sure about what to put in the vol_id //
-  //   db.Events.findAll ({
-  //     where: {
-  //       id: 2
-  //     },
-  //     attributes: ["event_name", "event_desc", "event_loc", "event_date", "event_time"]
-  //   }).then(function(db_vol_events) {
-  //     db.Volunteer_Events.findAll ({
-        
-  //     }).then(function(evnt_user){
-  //       res.json(evnt_user);
-  //     })
-  //   })
-    
-  // });
-
-  // app.get("/api/posts/", function(req, res) {
-  //   // console.log("POST GET: ", res)
-  //   db.Events.findAll({
-     
-  //   })
-  //     .then(function(dbEvents) {
-  //       res.json(dbEvents);
-  //       // console.log("Got the data: ", dbEvents);
-  //     });
-  // });
-
-
-
+  })
+}
 
 
 };
