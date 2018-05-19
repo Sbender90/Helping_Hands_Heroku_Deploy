05336942fa2be0bb58103449e320f6a59f08981e
Patrick Kearney <pckearney@yahoo.com>
2018-05-19 06:27:35 -0500

    Org page done minus restyling

--- app/public/js/organization.js
+++ app/public/js/organization.js
@@ -9,13 +9,11 @@ $(document).ready(function() {
     $.get("/api/org_events/" + hhOrgData.org_id, function(data) {
       if (data) {
         console.log(data);
-        console.log(data.length);
-
-        //Make a small table.
+        displayEvents(data);
       }
     });
   }
-
+  //Triggers the login process
   $("#org-login-btn").on("click", function(event) {
     event.preventDefault();
 
@@ -37,7 +35,7 @@ $(document).ready(function() {
       console.log("There are empty fields");
       return;
     }
-
+    //quesries database and returns with user info without password
     $.post("/api/login", newLogin, function(data) {
       if (data) {
         console.log(data);
@@ -47,11 +45,11 @@ $(document).ready(function() {
     });
   });
 
+  //Triggers the creation of a new event and refreshs the page.
   $("#new-event-btn").on("click", function(event) {
     event.preventDefault();
     //assemble an object defining an event
     var newEvent = {};
-
     console.log(newEvent);
 
     newEvent.event_name = $("#event-name")
@@ -69,7 +67,7 @@ $(document).ready(function() {
     newEvent.event_time = $("#event-time")
       .val()
       .trim();
-    newEvent.OrganizationOrgId = org_id;
+    newEvent.OrganizationOrgId = hhOrgData.org_id;
 
     document.getElementById("event-form").reset();
 
@@ -80,12 +78,15 @@ $(document).ready(function() {
       }
     });
 
-    //location.reload();
+    location.reload();
   });
 
+  //Triggers the display of volunteers signed up for an event.
   $("#event-volunteers-btn").on("click", function(event) {
     event.preventDefault();
     $("#vol-tbody").empty();
+    $("#vol-thead").empty();
+
     console.log("get my volunteers!");
 
     var event_id = $("#event-id-val")
@@ -103,27 +104,85 @@ $(document).ready(function() {
         }
       });
     }
-
-    //location.reload();
   });
-});
 
-function displayVolunteers(array) {
-  //empty out the current table
-  $("#vol-tbody").empty();
-  var tBody = $("#vol-tbody");
+  function displayVolunteers(array) {
+    //empty out the current table
+    $("#vol-thead").empty();
+    var headRow = $("<tr>");
+    var idTh = $("<th>")
+      .attr("scope", "col")
+      .html("#");
+    var firstNameTh = $("<th>")
+      .attr("scope", "col")
+      .html("First Name");
+    var lastNameTh = $("<th>")
+      .attr("scope", "col")
+      .html("Last Name");
+    var emailTh = $("<th>")
+      .attr("scope", "col")
+      .html("Email");
+    var phoneTh = $("<th>")
+      .attr("scope", "col")
+      .html("Phone Number");
+    headRow.append(idTh, firstNameTh, lastNameTh, emailTh, phoneTh);
+    $("#vol-thead").append(headRow);
 
-  for (i = 0; i < array.length; i++) {
-    var tRow = $("<tr>");
-    var headTh = $("<th>")
-      .attr("scope", "row")
-      .text(i + 1);
+    $("#vol-tbody").empty();
+    var tBody = $("#vol-tbody");
 
-    var firstNameTd = $("<td>").text(array[i].first_name);
-    var lastNameTd = $("<td>").text(array[i].last_name);
-    var emailTd = $("<td>").text(array[i].email);
-    var phoneTd = $("<td>").text(array[i].phone_num);
-    tRow.append(headTh, firstNameTd, lastNameTd, emailTd, phoneTd);
-    tBody.append(tRow);
+    for (i = 0; i < array.length; i++) {
+      var tRow = $("<tr>");
+      var headTh = $("<th>")
+        .attr("scope", "row")
+        .text(i + 1);
+
+      var firstNameTd = $("<td>").text(array[i].first_name);
+      var lastNameTd = $("<td>").text(array[i].last_name);
+      var emailTd = $("<td>").text(array[i].email);
+      var phoneTd = $("<td>").text(array[i].phone_num);
+      tRow.append(headTh, firstNameTd, lastNameTd, emailTd, phoneTd);
+      tBody.append(tRow);
+    }
   }
-}
+
+  function displayEvents(array) {
+    //empty out the current table
+    $("#event-thead").empty();
+    var headRow = $("<tr>");
+    var idTh = $("<th>")
+      .attr("scope", "col")
+      .html("event_id");
+    var nameTh = $("<th>")
+      .attr("scope", "col")
+      .html("Event Name");
+    var locationTh = $("<th>")
+      .attr("scope", "col")
+      .html("Event Location");
+    var dateTh = $("<th>")
+      .attr("scope", "col")
+      .html("Event Date");
+    var timeTh = $("<th>")
+      .attr("scope", "col")
+      .html("Event Time");
+    headRow.append(idTh, nameTh, locationTh, dateTh, timeTh);
+    $("#event-thead").append(headRow);
+
+    $("#event-tbody").empty();
+    var tBody = $("#event-tbody");
+
+    for (i = 0; i < array.length; i++) {
+      var tRow = $("<tr>");
+      var headTh = $("<th>")
+        .attr("scope", "row")
+        .text(array[i].event_id);
+
+      var nameTd = $("<td>").text(array[i].event_name);
+      var locationTd = $("<td>").text(array[i].event_loc);
+      var dateTd = $("<td>").text(array[i].event_date);
+      var timeTd = $("<td>").text(array[i].event_time);
+      tRow.append(headTh, nameTd, locationTd, dateTd, timeTd);
+      tBody.append(tRow);
+    }
+  }
+});
